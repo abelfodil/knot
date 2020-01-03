7d76e40e933d7fc55d405751fa180d3a18ec0b24
Anes Belfodil <ans.belfodil@gmail.com>
2020-01-03 14:37:05 -0500

    Add bluetooth utility functions

---
+++ bluetooth.py
@@ -0,0 +1,35 @@
+import shutil
+import subprocess
+
+bluetoothctl_path = shutil.which('bluetoothctl')
+bluetoothctl_exists = bluetoothctl_path != None
+
+
+class BluetoothctlDoesNotExist(Exception):
+    pass
+
+
+class BluetoothCommandError(Exception):
+    pass
+
+
+def execute_bluetoothctl_command(command: list):
+    if not bluetoothctl_exists:
+        raise BluetoothctlDoesNotExist("Please install bluez-utils package.")
+
+    result = subprocess.run([bluetoothctl_path, '--', *command], capture_output=True)
+    stdout = result.stdout.decode('utf-8')
+    if result.returncode != 0:
+        raise BluetoothCommandError(stdout)
+    else:
+        return stdout
+
+
+def get_device_info(mac_address: str):
+    return execute_bluetoothctl_command(['info', mac_address])
+
+
+def is_device_connected(mac_address: str):
+    device_info = get_device_info(mac_address).split('\n')
+    connected_info = [info for info in device_info if 'Connected:' in info][0]
+    return 'yes' in connected_info
